af7ef4da1b26f055d8195dbdc6473b2e9141e108
Enrick <enrico.verdolotti@gmail.com>
2014-06-22 21:46:06 +0200

    Inizio scrittura di address controller.

--- siw-project-asdrubale/src/it/uniroma3/controller/AddressController.java
+++ siw-project-asdrubale/src/it/uniroma3/controller/AddressController.java
@@ -1,5 +1,21 @@
 package it.uniroma3.controller;
 
+import it.uniroma3.facades.AdministrationFacade;
+import it.uniroma3.facades.ClientsFacade;
+
+import javax.ejb.EJB;
+import javax.faces.bean.RequestScoped;
+
+
+@RequestScoped
 public class AddressController {
+	
+	@EJB(beanName="aFacade")
+	private AdministrationFacade administrationFacade;
+	
+	@EJB(beanName="cFacade")
+	private ClientsFacade clientsFacade;
+	
+	
 
 }
